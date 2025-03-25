babc47b273055babd9b1ff1eef3b1546a5bf060b
akash06959 <187724245+akash06959@users.noreply.github.com>
2025-03-25 10:30:33 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1+5kk^%xwdb*e_iuxv71+ln%l)8l+x^)n*esth_s!+ilt96$k@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['render.com','localhost','*']
 
 
 # Application definition
